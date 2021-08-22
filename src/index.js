415797a1ce6e561f18a507665d92018d8feccd16
fe-luna <feluna.zhang@gmail.com>
2021-08-22 22:04:15 +0800

    feat: react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Luna')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Luna = React.createElement('div', null, 'Luna')
+const root = document.getElementById('app')
+
+ReactDOM.render(Luna, root)
