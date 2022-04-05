c7199d95d2ea0be1fc5549c3f825a39c48e863b7
fe-luna <feluna.zhang@gmail.com>
2022-04-05 22:29:56 +0800

    chore: webpack-bundle-analyzer

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
-const common = require('./webpack.config')
+const common = require('./webpack.common')
 
 module.exports = merge(common, {
   mode: 'development',
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge')
-const common = require('./webpack.config')
+const common = require('./webpack.common')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [process.env.BUNDLE_ANALYZER === 'true' && new BundleAnalyzerPlugin()].filter(Boolean),
 })
